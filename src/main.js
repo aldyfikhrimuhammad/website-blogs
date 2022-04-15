517aa66bd5750cb67f83cce326fef746bb0e56fb
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-04-15 08:59:43 +0700

    Uninstall Vuex

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
-import axios from "./plugins/axios";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import axios from './plugins/axios'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
   axios,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App)
+}).$mount('#app')
