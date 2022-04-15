2116a65dccbbc7557211e9b94a0edc43724dfdc1
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-04-15 09:37:10 +0700

    Try MapGetters and MapMutations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <!-- Container home page -->
   <v-container class="ma-0 pa-0" grid-list-sm>
-
     <!-- Button route to blogs -->
     <div class="text-right">
       <v-btn small text to="/blogs" class="blue--text"
@@ -9,11 +8,10 @@
         <v-icon left>mdi-chevron-right</v-icon>
       </v-btn>
     </div>
-    
-    <v-layout wrap>
-    <!-- Blog item // image & title -->
-      <v-flex v-for="blog in blogs" :key="`blog-` + blog.id" xs6>
 
+    <v-layout wrap>
+      <!-- Blog item // image & title -->
+      <v-flex v-for="blog in blogs" :key="`blog-` + blog.id" xs6>
         <v-card :to="`/blog/` + blog.id">
           <v-img :src="blog.photo ? apiDomain + blog.photo : 'https://picsum.photos/200/300'" class="white--text" height="200px">
             <v-card-title class="fill-height align-end" v-text="blog.title"></v-card-title>
@@ -27,36 +25,60 @@
             <span>{{ blog.title.substring(0, 15) }} ...</span>
           </v-card-actions>
         </v-card>
-
       </v-flex>
     </v-layout>
+    <Button @click="increment(100)">Tambah</Button>
+    {{ tambah }}
   </v-container>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from "vuex";
+
 export default {
-  
   data: () => ({
-    apiDomain: 'http://demo-api-vue.sanbercloud.com',
+    apiDomain: "http://demo-api-vue.sanbercloud.com",
     blogs: [],
   }),
 
+  computed: {
+    // Use Getters
+    // count() {
+    //   return this.$store.getters.count
+    // }
+
+    // Use MapGetters
+    ...mapGetters({
+      tambah: "count",
+    }),
+  },
+
   methods: {
-    go(){
+    go() {
       const config = {
-      method: 'get',
-      url:  this.apiDomain + '/api/v2/blog/random/4',
-    }
-    
-    this.axios(config)
-      .then(response => {
-        let { blogs } = response.data;
-        this.blogs = blogs;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    }
+        method: "get",
+        url: this.apiDomain + "/api/v2/blog/random/4",
+      };
+
+      this.axios(config)
+        .then((response) => {
+          let { blogs } = response.data;
+          this.blogs = blogs;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    // Use Mutations
+    // increment(payload) {
+    //   this.$store.commit('increment', payload)
+    // },
+
+    // Use MapMutations
+    ...mapMutations({
+      increment: "increment",
+    }),
   },
 
   created() {
